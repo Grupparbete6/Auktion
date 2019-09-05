b7ec6feaba2011b79b8321ad5f36c177762e8a4b
janapelsin <jan.mysiak@hotmail.com>
2019-09-05 12:07:44 +0200

    Skelett

---
+++ src/components/AuctionList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+// FUNCTIONS
+
+// sortAuctionArray()
+
+// selectAuction()
+
+export class AuctionList extends Component {
+    render() {
+        return (
+            <h1>Hello from Auction List!</h1>
+        )
+    }
+}
+
+export default AuctionList
---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+// FUNCTIONS
+
+// searchAuctionByName()
+
+export class Search extends Component {
+    render() {
+        return (
+            <h1>Hello from Search!</h1>
+        )
+    }
+}
+
+export default Search
