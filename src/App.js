c0aa9b8c0b30ff9a0586f26e60ac78994aaa75e0
janapelsin <jan.mysiak@hotmail.com>
2019-09-09 10:02:33 +0200

    Revert "Anrop OK. PUT kvar"
    This reverts commit ae1a8a0ea379767e46d529ed08ceb75774397afc.

--- src/App.js
+++ src/App.js
@@ -9,8 +9,6 @@ import AuctionDetails from './components/AuctionDetails';
 import CreateAuction from './components/CreateAuction';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-const url = 'http://nackowskis.azurewebsites.net/api/auktion/2150/';
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -21,14 +19,8 @@ export default class App extends React.Component {
     }
   }
 
-  // Såg så rörigt ut med denna, kanske finns en bättre lösning. La urlen längst upp så länge
-
-  // url = () => {
-  //   return "http://nackowskis.azurewebsites.net/api/auktion/2150/";
-  // }
-
-  // GET
   getData = async () => {
+    const url = 'http://nackowskis.azurewebsites.net/api/auktion/2150';
     const response = await fetch(url)
       .then(result => result.json());
     this.setState({
@@ -38,86 +30,35 @@ export default class App extends React.Component {
     console.log(this.state.AuctionInfo)
   }
 
-  // GET Auction by Id
-  getAuctionById = async (id) => {
-    const response = await fetch(url + id)
-      .then(result => result.json())
-    console.log(response)
-  }
-
-  // GET Bid by Auction Id
-  getBidByAuctionId = async (id) => {
-    var urlBid = url.replace("auktion", "bud")
-
-    const response = await fetch(urlBid + id)
-      .then(result => result.json())
-    console.log(response)
-  }
-
-  // POST Auction
-  createAuction = async () => {
-    
-    // Bör komma utifrån
-    var dummyObject = {
-      Titel: "TestAuktion",
-      Beskrivning: "Test från 1700-talet",
-      SkapadAv: "Test Exempelsson",
-      Gruppkod: 2150,
-      StartDatum: "2019-09-03T00:00:00",
-      SlutDatum: "2019-09-13T00:00:00",
-    };
-
-    fetch(url, {
-      method: 'POST',
-      body: JSON.stringify(dummyObject),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-      .then(response => console.log('Success:', JSON.stringify(response)))
-      .catch(error => console.error('Error:', error));
-  }
-
-  // POST Bid
-  placeBid = async (id) => {
-    var urlBid = url.replace("auktion", "bud")
-    // Bör komma utifrån
-    var newBid = {
-      AuktionId: id,
-      Summa: 999
-    }
-
-    fetch(urlBid, {
-      method: 'POST',
-      body: JSON.stringify(newBid),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(res => res.json())
-      .then(response => console.log('Success:', JSON.stringify(response)))
-      .catch(error => console.error('Error:', error));
-  }
-
-  // DELETE Auction
-  deleteAuction = async (id) => {
-    fetch(url + id, {
-      method: 'DELETE',
-    }).then(response => response.json())
-      .then(response => console.log('Success:', JSON.stringify(response)))
-      .catch(error => console.error('Error:', error));
-  }
-
   componentDidMount() {
     this.getData();
-    this.getAuctionById(4230);
-    this.getBidByAuctionId(4230);
-
-    // this.placeBid(4231);
-
-    //this.createAuction();
-    // this editAuction() <-- INTE IMPLEMENTERAD
-    //this.deleteAuction(4349)
   }
+
+
+
+  // stateSet = (data) => {
+  //   this.setState = ({
+  //     AuctionInfo: data
+  //   })
+  // }
+
+  // FUNCTIONS
+
+  // componentDidMount()
+  // GET from API
+  // this.setState({ todos: res.data })
+
+  // placeBid()
+  // PUT
+
+  // updateAuction()
+  // PUT
+
+  // createAuction()
+  // POST
+
+  // getAuctions()
+  // GET
 
 
   render() {
